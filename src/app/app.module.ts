3688d592f2991c477c427366f5b068dccd66c28a
Votre Nom <Vous@exemple.com>
2025-03-04 00:24:38 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent,
+    TaskListComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    
+    
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withFetch())
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/task.ts
@@ -0,0 +1,8 @@
+export interface Task{
+    id:number;
+    title:string;
+    description?:string;
+    startDateTime:string;
+    endDateTime:string;
+    status:'EN_ATTENTE' | 'EN_COURS' |'FAITE';
+}
